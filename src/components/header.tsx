4c07d423768e84ca6051fb237dcce023ff920d03
codeinee <>
2026-01-11 00:37:36 +0600

    prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,42 @@
-'use client';
+'use client'
 
-import Link from "next/link";
-import Image from "next/image";
-import {ROUTES} from "@/utils/routes.const";
-import {usePathname} from "next/navigation";
-import {cn} from "@/lib/css";
+import Link from 'next/link'
+import Image from 'next/image'
+import { ROUTES } from '@/utils/routes.const'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/css'
 
 export function Header() {
-    const pathname = usePathname()
+  const pathname = usePathname()
 
-    return <header>
-        <div className='main-container inner'>
-            <Link href={ROUTES.HOME}>
-                <Image src='/logo.svg' alt='Crypto logo' width={132} height={40} />
-            </Link>
+  return (
+    <header>
+      <div className="main-container inner">
+        <Link href={ROUTES.HOME}>
+          <Image src="/logo.svg" alt="Crypto logo" width={132} height={40} />
+        </Link>
 
-            <nav>
-                <Link href={ROUTES.HOME} className={cn('nav-link', {
-                    'is-active': pathname === ROUTES.HOME,
-                    'is-home': true
-                })}>Home</Link>
-                <p>Search Modal</p>
-                <Link href={ROUTES.COINS} className={cn('nav-link', {
-                    'is-active': pathname === ROUTES.COINS,
-                })}>All Coins</Link>
-            </nav>
-        </div>
+        <nav>
+          <Link
+            href={ROUTES.HOME}
+            className={cn('nav-link', {
+              'is-active': pathname === ROUTES.HOME,
+              'is-home': true,
+            })}
+          >
+            Home
+          </Link>
+          <p>Search Modal</p>
+          <Link
+            href={ROUTES.COINS}
+            className={cn('nav-link', {
+              'is-active': pathname === ROUTES.COINS,
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
     </header>
+  )
 }
--- src/utils/routes.const.ts
+++ src/utils/routes.const.ts
@@ -1,4 +1,4 @@
 export const ROUTES = {
-    HOME: '/',
-    COINS: '/coins'
+  HOME: '/',
+  COINS: '/coins',
 } as const
