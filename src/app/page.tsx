28d1ddf77399986b2e41263d3c302c41c4261834
codeinee <>
2026-01-11 00:31:40 +0600

    feat: implement navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className='text-indigo-500 text-3xl'>
-        PAGE
+    <div className=''>
+
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Link from "next/link";
+import Image from "next/image";
+import {ROUTES} from "@/utils/routes.const";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/css";
+
+export function Header() {
+    const pathname = usePathname()
+
+    return <header>
+        <div className='main-container inner'>
+            <Link href={ROUTES.HOME}>
+                <Image src='/logo.svg' alt='Crypto logo' width={132} height={40} />
+            </Link>
+
+            <nav>
+                <Link href={ROUTES.HOME} className={cn('nav-link', {
+                    'is-active': pathname === ROUTES.HOME,
+                    'is-home': true
+                })}>Home</Link>
+                <p>Search Modal</p>
+                <Link href={ROUTES.COINS} className={cn('nav-link', {
+                    'is-active': pathname === ROUTES.COINS,
+                })}>All Coins</Link>
+            </nav>
+        </div>
+    </header>
+}
